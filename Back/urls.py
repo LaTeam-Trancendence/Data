e5e5a2400c9bf1ace25cc27e03b8cf0aec9edc58
guillaume <vincent76t@gmx.fr>
2024-11-28 20:24:43 +0100

    correction

--- Back/urls.py
+++ Back/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/', include('register.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('friends.urls')),
     path('api/', include('stats.urls')),
     path('api/', include('player.urls'))
 ]
--- friends/serializers.py
+++ friends/serializers.py
@@ -3,7 +3,7 @@ from tables_core.models import CustomUser, Player
 
 
 class FriendSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source=Player.username)
+    username = serializers.CharField(source=Player.user)
     
     class Meta:
         model = Player
